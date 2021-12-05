411b547d1c932c095077994b8cd52187f0be9955
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2021-12-05 23:04:27 +0300

    created spring project

---
+++ Bas/src/main/java/com/umutyildiz/Bas/BasApplication.java
@@ -0,0 +1,13 @@
+package com.umutyildiz.Bas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasApplication.class, args);
+	}
+
+}
---
+++ Bas/src/test/java/com/umutyildiz/Bas/BasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umutyildiz.Bas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
